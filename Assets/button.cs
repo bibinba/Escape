3ac020cd82c46765e6de5aeb2a27b5b01102dcff
bibinba <bibi@gma.com>
2019-04-29 20:17:48 +0900

    画面遷移

---
+++ Assets/button.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class button : MonoBehaviour
+{
+    public Image image;
+    public Sprite[]room= new Sprite[4];
+
+
+    int roomnow = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.sprite = room[roomnow];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnRightButton()
+    {
+        
+       
+
+        if (0<=roomnow&& roomnow <= 2)
+        {
+            roomnow++;
+        }
+        else if (roomnow == 3)
+        {
+            roomnow = 0;
+        }
+
+        image.sprite = room[roomnow];
+        Debug.Log(roomnow);
+    }
+
+    public void OnLeftButton()
+    {
+       
+        if (1 <= roomnow && roomnow <= 3)
+        {
+            roomnow--;
+        }
+        else if (roomnow == 0)
+        {
+            roomnow = 3;
+        }
+        image.sprite = room[roomnow];
+        Debug.Log(roomnow);
+    }
+}
